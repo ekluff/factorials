a9cf62004521255687a742155998c3f8a8dd5c05
iman mafi & Evan Clough <student@epicodus.com>
2015-09-08 16:07:39 -0700

    Delete extra styling files and build index page.

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,38 @@ var factorial = function(num) {
     console.log(i);
     facArr.push(i);
   };
-
   var factor = 1;
 
   for (i=0; i < facArr.length; i++){
-    factor *= facArr[i];
+    factor = factor * facArr[i];
   };
 
-  return factor;
-
+  return(factor);
 };
 
-
-
-
-
-
-
-
 $(document).ready(function(){
+  $('form#factorial').submit(function(event) {
+    var facNum = parseInt($('input#number').val());
+    var facResult = factorial(facNum);
 
+    $('#facResult').text(facResult);
+    event.preventDefault();
+  });
 });
+
+
+// $("form#leap-year").submit(function(event) {
+//     var year = parseInt($("input#year").val());
+//     var result = leapYear(year);
+//
+//     $(".year").text(year);
+//     if (!result) {
+//       $(".not").text("not").show();
+//     } else {
+//       $(".not").hide()
+//     }
+//
+//     $("#result").show();
+//     event.preventDefault();
+//
+//   });
